78a86a31b5c297af8fc32a1de2ca37a9bf2e04d2
amit shakya <amit.shakya@webitude.com>
2018-01-15 17:37:38 +0530

    add post request

--- server.js
+++ server.js
@@ -27,6 +27,23 @@ app.get('/bookshelf/users',function(req,res){
 	})
 })
 
+app.post('/bookshelf/user',function(req,res){
+
+	console.log('post req',req.body)
+
+	users.forge({
+	name:'amit1',
+	email:'1amit0shakya@gmail.com'
+	})
+	.save()
+	.then(function(user){
+	res.json({error:false,data:user})
+	})
+	.catch(function(err){
+	res.status(500).json({error:true,data,err})
+	})
+})
+
 app.listen(3000,function(){
 	console.log('server running port '+3000)
 })
